891dee55bebafd5726fd618eee8c80f9c1c348ed
ahonafredowanhasib <4hon4f.redowan.hasib.03@gmail.com>
2025-09-23 02:22:01 +0600

    2nd commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const loadLessons =()=>{
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then(res=>res.json())
+    .then(json => displayLessons(json.data))
+}
+ const displayLessons =(lessons)=>{
+    // 1. get the container and empty
+ const levelContainer = document.getElementById("level-container")
+ levelContainer.innerHTML="";
+
+    // 2. get into every lessons
+for(let lesson of lessons){
+
+    // 3. create element
+const btnDiv = document.createElement("div")
+btnDiv.innerHTML=`
+    <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
+`
+// 4. append into container
+levelContainer.append(btnDiv)
+}
+
+    
+ }
+
+loadLessons()
